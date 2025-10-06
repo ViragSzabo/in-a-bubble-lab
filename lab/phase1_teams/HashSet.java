65294f9e3571721033c5f79b21d913d81ca575c6
ViragSzabo <virag.szabo@student.nhlstenden.com>
2025-10-06 15:55:00 +0200

    Add HashSet and Stack.

--- lab/phase1_teams/HashSet.java
+++ lab/phase1_teams/HashSet.java
@@ -1,4 +1,38 @@
 package phase1_teams;
 
-public class HashSet {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashSet<T> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private List<T>[] buckets;
+
+    public HashSet() {
+        this.buckets = new List[DEFAULT_CAPACITY];
+        for(int i = 0; i < DEFAULT_CAPACITY; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+    }
+
+    private int getBucketIndex(T data) {
+        return (data.hashCode() & 0x7fffffff) % this.buckets.length;
+    }
+
+    public boolean add(T data) {
+        int index = getBucketIndex(data);
+        if(buckets[index].contains(data)) {
+            return false;
+        } else {
+            buckets[index].add(data);
+            return true;
+        }
+    }
+
+    public boolean remove(T data) {
+        return buckets[getBucketIndex(data)].remove(data);
+    }
+
+    public boolean contains(T data) {
+        return buckets[getBucketIndex(data)].contains(data);
+    }
 }
--- lab/phase1_teams/Stack.java
+++ lab/phase1_teams/Stack.java
@@ -1,4 +1,29 @@
 package phase1_teams;
 
-public class Stack {
+import java.util.NoSuchElementException;
+
+public class Stack<T> {
+    private Node<T> top;
+    private int size;
+
+    private static class Node<T>{
+        private T data;
+        private Node<T> next;
+        public Node(T data, Node<T> next) { this.data = data; }
+    }
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+
+    public T pop(){
+        if(isEmpty()) throw new NoSuchElementException("Stack is empty");
+        T data = top.data;
+        size--;
+        return top.data;
+    }
+
+    public boolean isEmpty() { return true; }
+    public int size() { return size; }
 }
